d89d0c9dd1d37ecf943c8fb468fa6165d7574d42
Troy Dorman <troydorman@gmail.com>
2021-09-25 19:24:57 -0600

    created logs table with columns in our database

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import errorcode
 from database import cursor
 
 DB_NAME = 'acme'
@@ -19,4 +20,21 @@ def create_database():
     cursor.execute("CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
     print("Databased {} created!".format(DB_NAME))
 
+def create_tables():
+    cursor.execute("USE {}".format(DB_NAME))
+
+    for table_name in TABLES:
+        table_description = TABLES[table_name]
+        try:
+            print("Creating table ({})" .format(table_name), end="")
+            cursor.execute(table_description)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("already exists.")
+            else:
+                print(err.msg)
+
+
+
 create_database()
+create_tables()
