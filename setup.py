7ee84e2ce7b41c7b96b87adf48fed053f5bdd084
Troy Dorman <troydorman@gmail.com>
2021-09-25 19:19:00 -0600

    defined our logs table schema

--- setup.py
+++ setup.py
@@ -3,9 +3,20 @@ from database import cursor
 
 DB_NAME = 'acme'
 
+TABLES = {}
+
+TABLES['logs'] = (
+    "CREATE TABLE `logs` ("
+    " `id` int(11) NOT NULL AUTO_INCREMENT,"
+    " `text` varchar(250) NOT NULL,"
+    "  `user` varchar(250) NOT NULL,"
+    "`created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+    " PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB"
+)
+
 def create_database():
     cursor.execute("CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
     print("Databased {} created!".format(DB_NAME))
 
 create_database()
-    
